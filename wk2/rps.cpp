7d5bdf9d462f97f6b539acc7fdc93722f635e386
Elise Soto <w624q859@wichita.edu>
2026-04-07 10:40:05 -0500

    rock paper scissors program

---
+++ wk2/rps.cpp
@@ -0,0 +1,16 @@
+// Elise Soto Coding 1 Spring 2026
+// Random Numbers and Switch Statements and While Loops
+
+// compile with the Developer Command Prompt
+    // cd C:/
+    // C:\Users\skzki\GitHub\Coding1B_SPT6_CPP\Coding1B_SPT6_CPP\wk1
+    // 'cl /EHsc rand.cpp' is what you type to compile
+    // 'rand' or 'rand.exe' to run the program
+// Fill out the rest of the code so it displays "Rock Paper Scissors" to the console
+// Then run it
+
+#include iostream
+
+int main () {
+    cout << "Let's play rock, paper, scissors!\n";
+}
