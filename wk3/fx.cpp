ebccb48b6235011c1e643247f896925e5f6f4186
Elise Soto <w624q859@wichita.edu>
2026-04-16 14:24:07 -0500

    pt 2

--- wk3/fx.cpp
+++ wk3/fx.cpp
@@ -101,7 +101,7 @@ int main(){
     cout << "Finally, functions!\n";
 
     showVec(names);
-    addStringToVec(names);
+    addStringToVec();
     sayHello();
 
     cout << "Let's roll a dice. Our first number is " << dieRoll(6) << ".\n";
